33a2a697fc0eab833d83ed987a2dd19198830212
tobiu <tobiasuhlig78@gmail.com>
2024-06-10 13:20:47 +0200

    copied & adjusted the apps/colors code from the neo repo

--- apps/colors/app.mjs
+++ apps/colors/app.mjs
@@ -1,6 +1,6 @@
-import MainContainer from './view/MainContainer.mjs';
+import Viewport from './view/Viewport.mjs';
 
 export const onStart = () => Neo.app({
-    mainView: MainContainer,
-    name    : 'colors'
+    mainView: Viewport,
+    name    : 'Colors'
 });
